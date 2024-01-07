20a0aaa831257d2ea2243220e21cababca2f8bdb
Felipe Paskevicius <84820517+mouraxy@users.noreply.github.com>
2024-01-06 23:30:50 -0300

    Update formatar_tabela.py

--- modelos/formatar_tabela.py
+++ modelos/formatar_tabela.py
@@ -13,3 +13,32 @@ def formatar__tabela(df):
         floatfmt=".2f"
     )
     return tb
+
+
+def gerar__metadados(df):
+    '''
+    Retorna metadados de uma tabela
+    Parametros:
+    df:pd.DataFrame, a tabela de onde os metadados serao gerados.
+    '''
+    # obtem os tipos de dados
+    tipo_dados = df.dtypes
+
+    # calcula a quantidade de nulos
+    qtde_nulos = df.isnull().sum()
+    estatisticas = df.describe(include='all').transpose()
+
+    # cria um dataframe com algumas estatisticas
+    df_metadados = pd.DataFrame({
+    'Variável': tipo_dados.index,
+    'Tipos de dados': tipo_dados.values,
+    'Nulos': qtde_nulos.values,
+    'Mínimo': estatisticas['min'],
+    'Máximo': estatisticas['max'],
+    'Média': estatisticas['mean'],
+    })
+
+    print(f'\n\nO DataFrame possui {df.shape[1]} colunas e {df.shape[0]} linhas:')
+    print(formatar__tabela(df_metadados))
+
+gerar__metadados(df_origem)
